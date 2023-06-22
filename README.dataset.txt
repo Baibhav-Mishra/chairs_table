9ae813b4b576b175a5fcea381af1929ce6e03719
Baibhav Mishra <baibhavmishra2010@gmail.com>
2023-06-22 10:46:46 +0530

    new

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# first > 2023-06-22 10:44am
+https://universe.roboflow.com/baibhav-mishra-95kci/first-ggmwl
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ hi.py
@@ -0,0 +1,51 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+#
+# Load 50 random samples from the validation split
+#
+# Only the required images will be downloaded (if necessary).
+# By default, only detections are loaded
+#
+
+dataset = foz.load_zoo_dataset(
+    "coco-2017",
+    split="validation",
+    max_samples=50,
+    shuffle=True,
+    only_matching=True,
+    classes=["chair", "dining table"],
+    label_types=['detections']
+)
+
+session = fo.launch_app(dataset)
+
+#
+# Load segmentations for 25 samples from the validation split that
+# contain cats and dogs
+#
+# Images that contain all `classes` will be prioritized first, followed
+# by images that contain at least one of the required `classes`. If
+# there are not enough images matching `classes` in the split to meet
+# `max_samples`, only the available images will be loaded.
+#
+# Images will only be downloaded if necessary
+#
+
+# dataset = foz.load_zoo_dataset(
+#     "coco-2017",
+#     split="validation",
+#     label_types=["segmentations"],
+#     classes=["cat", "dog"],
+#     max_samples=25,
+# )
+
+# session.dataset = dataset
+
+# dataset = foz.load_zoo_dataset(
+#     "coco-2017",
+#     split="validation",
+#     label_types=["detections", "segmentations"],
+# )
+
+# session.dataset = dataset
